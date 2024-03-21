39838f79527536b3b35eb52e7d094e375c6423de
Yifan Zeng <zyf6@outlook.com>
2024-03-20 22:44:53 -0700

    update music experiment script

--- collect_result.py
+++ collect_result.py
@@ -1,5 +1,49 @@
 import pandas as pd
+import re
 
-df = pd.read_csv("logs/results.txt", names=["exp_name", "seed", "test_accuracy"])
-df = df.groupby("exp_name").mean()
-print(df)
+def prase_exp_name(exp_name):
+    # food-syn_F_lambda0.001_gamma0.2_model-resnet50
+    exp_name_split = exp_name.split("_")
+    # use regex to extract lambda and gamma
+    lambda_value = re.search(r"lambda([.\d]+)", exp_name)
+    lambda_value = lambda_value.group(1) if lambda_value else None
+    gamma_value = re.search(r"gamma([.\d]+)", exp_name)
+    gamma_value = gamma_value.group(1) if gamma_value else None
+    model_name = re.search(r"model-(\w+)", exp_name)
+    model_name = model_name.group(1) if model_name else None
+    if model_name is None and exp_name_split[0] == "cifar-syn":
+        model_name = "resnet9"
+    details = {
+        "dataset": exp_name_split[0],
+        "crowd_config": exp_name_split[1],
+        "lambda": lambda_value,
+        "gamma": gamma_value,
+        "model": model_name
+    }
+    return details
+
+
+def main():
+    df = pd.read_csv("logs/results.txt", names=["exp_name", "seed", "test_accuracy"])
+    df["seed"] = df["seed"].astype(int)
+
+    # parse experiment name
+    df["exp_details"] = df["exp_name"].apply(prase_exp_name)
+    df = df.join(pd.json_normalize(df["exp_details"]))
+
+    # remove duplicates
+    print("Experiments Count", len(df))
+    df = df.drop_duplicates(subset=["dataset", "crowd_config", "lambda", "gamma", "model", "seed"])
+    print("Unique experiments: ", len(df))
+
+    # aggregate different seeds and calculate mean and std
+    df = df.groupby(["dataset", "crowd_config", "lambda", "gamma", "model"]).agg(
+        mean_accuracy=("test_accuracy", "mean"),
+        std_accuracy=("test_accuracy", "std"),
+    ).reset_index()
+
+    df.to_csv("logs/results.csv", index=False)
+
+
+if __name__ == '__main__':
+    main()
--- scripts/run_music.sh
+++ scripts/run_music.sh
@@ -1,13 +1,14 @@
+for lam in 0.0001 0.001 0.01
+do
 for seed in 0 1 2 3 4
 do
-    python main.py --accelerator cpu --experiment_name music_F \
-    --regularization_type F --K 10 --lambda_reg 0.01 --batch_size 64 --n_epoch 1000 --seed $seed &
-
-    python main.py --accelerator cpu --experiment_name music_no \
-    --K 10 --regularization_type no --n_epoch 1000 --batch_size 64 --seed $seed &
-
-    python main.py --accelerator cpu --experiment_name music_W \
-    --regularization_type W --K 10 --lambda_reg 0.01 --batch_size 64 --n_epoch 1000 --seed $seed &
-
-    wait
+for type in W F no
+do
+python main.py --accelerator cpu \
+--experiment_name "music_${type}_lambda${lam}" \
+--regularization_type $type --K 10 --M 44 \
+--lambda_reg $lam --batch_size 64 --n_epoch 1000 --seed $seed &
+done
+done
+wait
 done
